24010bbbe9580c2fa9619a240131dc721df49b99
Graves, Mark <mark.graves@tylertech.com>
2019-08-13 12:10:48 -0500

    Added new endpoints to support API calls

--- check-yo-self-indexer/Controllers/EmployeesController.cs
+++ check-yo-self-indexer/Controllers/EmployeesController.cs
@@ -41,6 +41,149 @@ namespace check_yo_self_indexer.Server.Controllers.api
             _elasticClient = new ElasticClient(elasticSettings);
         }
 
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Employee>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                // Search for the document using the employeeId field.             
+                var searchResponse = await _elasticClient.SearchAsync<Employee>(s => s
+                    .Index(_appConfig.Elasticsearch.IndexName)
+                    .Size(1000)
+                    .Query(q => q
+                        .MatchAll()
+                    )
+                    .Sort(so => so
+                        .Ascending(a => a.LastName)
+                        .Ascending(a => a.FirstName)
+                        .Ascending(a => a.EmployeeId))
+                );
+
+                var employees = searchResponse.Documents.ToList();
+
+                return Ok(employees);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(1, ex, "Unable to load all employees");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{employeeId}")]
+        [ProducesResponseType(typeof(Employee), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int employeeId)
+        {
+            try
+            {
+                // Search for the document using the employeeId field.             
+                var searchResponse = await _elasticClient.SearchAsync<Employee>(s => s
+                    .Index(_appConfig.Elasticsearch.IndexName)
+                    .Size(1000)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.EmployeeId)
+                            .Query(employeeId.ToString())
+                        )
+                    )
+                );
+
+                // If found, delete the document
+                if (searchResponse.Hits.Count() > 0)
+                {
+                    return Ok(searchResponse.Documents.FirstOrDefault());
+                }
+                else
+                {
+                    // If not found, indicate as such
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(1, ex, "Unable to load document with employeeId: " + employeeId);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{lastName}")]
+        [ProducesResponseType(typeof(List<Employee>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByLastName(string lastName)
+        {
+            try
+            {
+                // Search for the document using the employeeId field.             
+                var searchResponse = await _elasticClient.SearchAsync<Employee>(s => s
+                    .Index(_appConfig.Elasticsearch.IndexName)
+                    .Size(1000)
+                    .Query(q => q
+                        .Match(m => m
+                            .Field(f => f.LastName)
+                            .Query(lastName)
+                        )
+                    )
+                );
+
+                // If found, delete the document
+                if (searchResponse.Hits.Count() > 0)
+                {
+                    return Ok(searchResponse.Documents.ToList());
+                }
+                else
+                {
+                    // If not found, indicate as such
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(1, ex, "Unable to load documents with lastName: " + lastName);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        [HttpGet("{firstName}/{lastName}")]
+        [ProducesResponseType(typeof(List<Employee>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByFirstAndLastName(string firstName, string lastName)
+        {
+            try
+            {
+                // Search for the document using the employeeId field.             
+                var searchResponse = await _elasticClient.SearchAsync<Employee>(s => s
+                    .Index(_appConfig.Elasticsearch.IndexName)
+                    .Size(1000)
+                    .Query(q => q
+                        .Term(t => t.LastName, lastName) && q
+                        .Term(t => t.FirstName, firstName)                            
+                    )
+                );
+
+                // If found, delete the document
+                if (searchResponse.Hits.Count() > 0)
+                {
+                    return Ok(searchResponse.Documents.ToList());
+                }
+                else
+                {
+                    // If not found, indicate as such
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(1, ex, "Unable to load documents with lastName: " + lastName);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
