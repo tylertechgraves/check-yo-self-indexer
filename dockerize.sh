caed280ee4f2149c767c98e4cb30d47ebb1e5565
Graves, Mark <mark.graves@tylertech.com>
2019-08-15 10:57:37 -0500

    Added dockerize bash script

---
+++ dockerize.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ./check-yo-self-indexer || exit
+dotnet publish -c Debug -r linux-musl-x64
+docker build --build-arg Configuration=Debug -t check-yo-self-indexer:1.0.1 .
+cd .. || exit
