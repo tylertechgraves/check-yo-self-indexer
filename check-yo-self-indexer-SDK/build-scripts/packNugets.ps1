474c1002a14e1cae0534827b26b73b8189a8f7d3
Graves, Mark <mark.graves@tylertech.com>
2019-08-15 10:58:12 -0500

    Refactored packNugets.ps1 script to run from its own directory

--- check-yo-self-indexer-SDK/build-scripts/packNugets.ps1
+++ check-yo-self-indexer-SDK/build-scripts/packNugets.ps1
@@ -1,10 +1,3 @@
-Write-Host "Script was originally running in: " $(Get-Location)
-Push-Location
-Set-Location $(Join-Path $PSScriptRoot ..)
-Write-Host "Script has changed location to: " $(Get-Location)
-
-dotnet publish ../check-yo-self-indexer/check-yo-self-indexer.csproj -c Release -r linux-musl-x64
-dotnet publish ./check-yo-self-indexer-sdk.csproj -o ./out
-dotnet pack ./check-yo-self-indexer-sdk.csproj /p:PackageVersion=1.0.7-beta --configuration Debug --include-source --include-symbols --output ./nupkg --version-suffix "beta"
-
-Pop-Location
+dotnet publish ../../check-yo-self-indexer/check-yo-self-indexer.csproj -c Release -r linux-musl-x64
+dotnet publish ../check-yo-self-indexer-sdk.csproj -o ./out
+dotnet pack ../check-yo-self-indexer-sdk.csproj /p:PackageVersion=1.0.7-beta --configuration Debug --include-source --include-symbols --output ./nupkg --version-suffix "beta"
