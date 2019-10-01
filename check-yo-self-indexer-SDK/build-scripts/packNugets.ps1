526ee17f1f7367457772343087581271f5cf3d6f
Graves, Mark <mark.graves@tylertech.com>
2019-10-01 14:54:11 -0500

    Fixed path issues in packNugets scripts

--- check-yo-self-indexer-SDK/build-scripts/packNugets.ps1
+++ check-yo-self-indexer-SDK/build-scripts/packNugets.ps1
@@ -1,3 +1,3 @@
 dotnet publish ../../check-yo-self-indexer/check-yo-self-indexer.csproj -c Release -r linux-musl-x64
-dotnet publish ../check-yo-self-indexer-sdk.csproj -o ./out
-dotnet pack ../check-yo-self-indexer-sdk.csproj /p:PackageVersion=1.0.7-beta --configuration Debug --include-source --include-symbols --output ./nupkg --version-suffix "beta"
+dotnet publish ../check-yo-self-indexer-sdk.csproj -o ../out
+dotnet pack ../check-yo-self-indexer-sdk.csproj /p:PackageVersion=1.0.7-beta --configuration Debug --include-source --include-symbols --output ../nupkg --version-suffix "beta"
