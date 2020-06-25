d0f7c8165df9c3cc64d4a5f607aad1407e77032c
Graves, Mark <mark.graves@tylertech.com>
2020-06-25 14:25:29 -0500

    chore: Changed docker image version back to 1.0.0

--- dockerize.ps1
+++ dockerize.ps1
@@ -1,4 +1,4 @@
 Set-Location ./check-yo-self-indexer
 dotnet publish -c Debug -r linux-musl-x64
-docker build --build-arg Configuration=Debug -t check-yo-self-indexer:1.0.1 .
+docker build --build-arg Configuration=Debug -t check-yo-self-indexer:1.0.0 .
 Set-Location ../
